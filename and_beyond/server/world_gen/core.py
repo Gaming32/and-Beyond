96799d304a90ec6a72a17cbe23fd4f660f16fa68
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2022-06-29 14:50:05 -0500

    Move import

--- and_beyond/server/world_gen/core.py
+++ and_beyond/server/world_gen/core.py
@@ -3,6 +3,7 @@ from typing import TYPE_CHECKING
 from and_beyond.server.world_gen.phase import AbstractPhase
 from and_beyond.server.world_gen.phases.caves import CavePhase
 from and_beyond.server.world_gen.phases.decorations import TreeDecorationPhase
+from and_beyond.server.world_gen.phases.ground import GroundPhase
 from and_beyond.server.world_gen.phases.sky_islands import SkyIslandsPhase
 from and_beyond.utils import autoslots
 
@@ -12,7 +13,7 @@ if TYPE_CHECKING:
 @autoslots
 class WorldGenerator:
     seed: int
-    ground: 'GroundPhase'
+    ground: GroundPhase
     phases: list[AbstractPhase]
 
     def __init__(self, seed: int) -> None:
@@ -28,6 +29,3 @@ class WorldGenerator:
     def generate_chunk(self, chunk: 'WorldChunk') -> None:
         for phase in self.phases:
             phase.generate_chunk(chunk)
-
-
-from and_beyond.server.world_gen.phases.ground import GroundPhase
